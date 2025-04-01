6065ec33cfd95116fda8872cff18048a97b56f85
yassmineabdallah <yesmeeen.abdallah@gmail.com>
2025-04-01 01:02:19 +0100

    gitignore

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
- 
+import os
+
+# Chemin de l'image d'entrée et de sortie
+input_image_path = r'test\iii1.jpg'
+output_directory = r'sortie'
+output_image_path = os.path.join(output_directory, 'image_amelioree.jpg')
 
 # Chargement de l'image
-image = cv2.imread('test\iii1.jpg', cv2.IMREAD_COLOR)
+def load_image(image_path):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    if image is None:
+        raise FileNotFoundError(f"L'image {image_path} n'a pas été trouvée.")
+    return image
 
-
-# Affichage de l'image originale
-plt.imshow(image, cmap='gray')
-plt.title("Image originale")
-plt.show()
 # Égalisation de l'histogramme
-image_eq = cv2.equalizeHist(image)
+def histogram_equalization(image):
+    return cv2.equalizeHist(image)
 
-
-# Affichage de l'image après égalisation
-plt.imshow(image_eq, cmap='gray')
-plt.title("Image après égalisation")
-plt.show()
 # Seuillage d'Otsu
-_, image_otsu = cv2.threshold(image_eq, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+def otsu_threshold(image):
+    _, thresholded = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return thresholded
 
-
-# Affichage de l'image après seuillage d'Otsu
-plt.imshow(image_otsu, cmap='gray')
-plt.title("Image après seuillage d'Otsu")
-plt.show()
 # Amélioration de la luminosité
-image_bright = cv2.convertScaleAbs(image_eq, alpha=1, beta=50)
+def enhance_brightness(image, beta=50):
+    return cv2.convertScaleAbs(image, alpha=1, beta=beta)
 
+# Affichage des images
+def show_image(image, title):
+    plt.imshow(image, cmap='gray')
+    plt.title(title)
+    plt.axis('off')
+    plt.show()
 
-# Affichage de l'image avec plus de luminosité
-plt.imshow(image_bright, cmap='gray')
-plt.title("Image avec plus de luminosité")
-plt.show()
+# Pipeline de traitement
+image = load_image(input_image_path)
+show_image(image, "Image originale")
+
+image_eq = histogram_equalization(image)
+show_image(image_eq, "Image après égalisation de l'histogramme")
+
+image_otsu = otsu_threshold(image_eq)
+show_image(image_otsu, "Image après seuillage d'Otsu")
+
+image_bright = enhance_brightness(image_eq)
+show_image(image_bright, "Image avec amélioration de la luminosité")
+
 # Sauvegarde de l'image améliorée
-cv2.imwrite('image_amelioree.jpg', image_bright)
+cv2.imwrite(output_image_path, image_bright)
+print(f"L'image améliorée a été sauvegardée sous {output_image_path}")
